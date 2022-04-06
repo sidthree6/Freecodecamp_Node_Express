0a64dde1d7fd12e12f31bda5255b1ef60116836f
sidthree6 <psiddhu001@gmail.com>
2022-04-06 09:22:35 -0400

    modules and mind grenade

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// Module
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavours')
+require('./7-mind-grenade')
+
+// sayHi('Sidd')
+// sayHi(names.john)
+// sayHi(names.peter)
