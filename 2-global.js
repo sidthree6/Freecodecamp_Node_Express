3e52b69d20e0e819bd2685cf20295ab4171e1520
sidthree6 <psiddhu001@gmail.com>
2022-04-06 08:25:15 -0400

    Adding files which were already made.

---
+++ 2-global.js
@@ -0,0 +1,8 @@
+//  GLOBALS
+// __dirname
+// __filename
+// require
+// module
+// process
+
+console.log(module)
---
+++ app.js
@@ -0,0 +1,7 @@
+// Module
+const names = require('./4-names');
+const sayHi = require('./5-utils')
+
+sayHi('Sidd')
+sayHi(names.john)
+sayHi(names.peter)
